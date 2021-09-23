77d7aa2f3ce8dd31bec35937e7f05be035a35db7
krisszzzz <89372621+krisszzzz@users.noreply.github.com>
2021-09-23 10:21:40 +0300

    Add files via upload

--- Onegin.c
+++ Onegin.c
@@ -1,99 +1,145 @@
 ﻿// Onegin.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
 #define WINDOWS
 #pragma warning (disable: 4996)
 
-enum 
-{
-	TRANSLATE_INDEX_TO_SIZE = 1
-};
-struct string
-{
-	size_t line_size;
-	char* line;
-};
-
-size_t find_file_size(FILE* source);
-
-char* read_to_f_buffer(FILE* source, size_t file_size);
-
-size_t count_strings(char* buffer, size_t buffer_size);
-
-void read_strings(char* buffer, struct string* str, size_t num_of_string);
-
-char* strndup(char* src, size_t size);
-
-void my_qsort(void* first, int num, int size, int (*compar)(void*, void*));
-
-int comparator(struct string* first, struct string* second);
-
-void swap(void* a, void* b, int size);
-
-void output(struct string* ptr_string, size_t num_of_string, FILE* dst);
-
-void free_memory(char* f_buffer, struct string* ptr_string, size_t num_of_string);
+#include "Header.h"
 
 int main()
 {
+	FILE* LOG_FILE = fopen("LOG_FILE.txt", "w");
+
 
 	FILE* source = fopen("Translated_Onegin.txt", "rb");
 
-	size_t file_size = find_file_size(source);
+	ptr_status(source, "Error: the file isn't open\n", LOG_FILE);
+
+	long_int file_size = find_file_size(source);
 
 	char* f_buffer = read_to_f_buffer(source, file_size);
+	ptr_status(f_buffer, "Error: incorrect file reading\n", LOG_FILE);
 
-	size_t num_of_strings = count_strings(f_buffer, file_size);
+	long_int num_of_strings = count_strings(f_buffer, file_size);
 
-	struct string* ptr_string = calloc(num_of_strings, sizeof(struct string));
+	string* ptr_string = calloc(num_of_strings, sizeof(string));
+
+	ptr_status(ptr_status, "Incorrect memory allocation for string structure\n", LOG_FILE);
 
 	read_strings(f_buffer, ptr_string, num_of_strings);
-
-	my_qsort(ptr_string, num_of_strings, sizeof(struct string), comparator);
+	
+	//my_qsort(ptr_string, num_of_strings, sizeof(string), comparator);
+	my_qsort(ptr_string, num_of_strings, sizeof(string), (int(*)(void*, void*)) end_comparator);
 
 	FILE* destination = fopen("True_Onegin.txt", "wb");
 	output(ptr_string, num_of_strings, destination);
 
+	//my_qsort(ptr_string, num_of_strings, sizeof(string), end_comparator);
+	//output(ptr_string, num_of_strings, destination);
+
+	fclose(source);
+	fclose(destination);
+
 	free_memory(f_buffer, ptr_string, num_of_strings);
 }
-int comparator(struct string* first, struct string* second)
+
+void ptr_status(void* ptr, const char* error, FILE* log_file)
 {
-	while (!isalpha(*(first->line)))
-		++(first->line);
-
-	while (!isalpha(*((*second).line)))
-		++(second->line);
-
-	for (int i = 0; tolower(first->line[i]) == tolower(second->line[i]); ++i)
-		if (second->line[i] == '\0')
-			return 0;
-
-	return tolower(*((*first).line)) - tolower(*((*second).line));
+	if (ptr == NULL)
+	{
+		fprintf(log_file, error);
+		assert(NULL);
+	}
 }
-size_t find_file_size(FILE* source)
+
+int comparator(string* first, string* second)
+{
+	char* p_first = first->line;
+	char* p_second = second->line;
+	int is_equal = 1;
+	while(*first->line != '\0' && *second->line != '\0')
+	{
+		if (!isalpha(*first->line))
+			++first->line;
+		if(!isalpha(*second->line))
+			++second->line;
+
+		if (isalpha(*first->line) && isalpha(*second->line))
+		{
+			if (*first->line != *second->line)
+				break;
+			else
+			{
+				++first->line;
+				++second->line;
+			}
+		}
+	}
+	int c = tolower(*first->line) - tolower(*second->line);
+	first->line = p_first;
+	second->line = p_second;
+
+	return c;
+}
+int end_comparator(string* first, string* second)
+{
+	char* p_first = first->line;
+	char* p_second = second->line;
+
+	first->line += first->line_size - 1;
+	second->line += second->line_size - 1;
+
+	int is_equal = 1;
+	while (*first->line != *p_first && *second->line != *p_second)
+	{
+		if (!isalpha(*first->line))
+			--first->line;
+		if (!isalpha(*second->line))
+			--second->line;
+
+		if (isalpha(*first->line) && isalpha(*second->line))
+		{
+			if (*first->line != *second->line)
+				break;
+			else
+			{
+				--first->line;
+				--second->line;
+			}
+		}
+	}
+	int c = tolower(*first->line) - tolower(*second->line);
+	first->line = p_first;
+	second->line = p_second;
+
+	return c;
+}
+
+long_int find_file_size(FILE* source)
 {
 	fseek(source, 0, SEEK_END);
-	size_t file_size = ftell(source);
+	long_int file_size = ftell(source);
 	fseek(source, 0, SEEK_SET);
 
 	return file_size;
 }
-char* read_to_f_buffer(FILE* source, size_t file_size)
+char* read_to_f_buffer(FILE* source, long_int file_size)
 {
-	char* f_buffer = calloc(file_size + 1, sizeof(char));
-	fread(f_buffer, sizeof(char), file_size, source);
-	f_buffer[file_size] = '\0';
+	char* f_buffer = calloc((size_t)file_size + 1, sizeof(char));
+
+	if (f_buffer != NULL)
+	{
+		fread(f_buffer, sizeof(char), file_size, source);
+		f_buffer[file_size] = '\0';
+	}
+	else return NULL;
 
 	return f_buffer;
 }
-size_t count_strings(char* buffer, size_t buffer_size)
+long_int count_strings(char* buffer, long_int buffer_size)
 {
-	size_t i = 0;
-	size_t num_of_strings = 0;
+	long_int i = 0;
+	long_int num_of_strings = 0;
 	while(i < buffer_size)
 	{
 		if(buffer[i] == '\n')
@@ -103,7 +149,7 @@ size_t count_strings(char* buffer, size_t buffer_size)
 	return num_of_strings;
 }
 
-void read_strings(char* buffer, struct string* str, size_t num_of_string)
+void read_strings(char* buffer, string* str, long_int num_of_string)
 {
 	#ifdef WINDOWS
 		int num_of_ignored_char = 2;
@@ -111,13 +157,17 @@ void read_strings(char* buffer, struct string* str, size_t num_of_string)
 		int num_of_ignored_char = 1;
 	#endif
 	
-	int string_index = 0;
-	size_t readed_string_size = 0;
+	long_int string_index = 0;
+	long_int readed_string_size = 0;
 	char c;
-	size_t size_of_string = 0;
+	long_int size_of_string = 0;
 	
-	for(size_t buf_index = 0; (c =  buffer[buf_index]) != '\0'; ++buf_index)
+	for(long_int buf_index = 0; (c =  buffer[buf_index]) != '\0'; ++buf_index)
 	{
+		if (c < 0)
+		{
+
+		}
 		if(c == '\n')
 		{
 			
@@ -134,11 +184,12 @@ void read_strings(char* buffer, struct string* str, size_t num_of_string)
 
 	}
 }
-char* strndup(char* src, size_t size)
-{
-	size_t i = 0;
 
-	char* dst = calloc(size + 1, sizeof(char));
+char* strndup(char* src, long_int size)
+{
+	long_int i = 0;
+
+	char* dst = calloc((size_t)size + 1, sizeof(char));
 
 	while(i < size)
 	{
@@ -150,9 +201,9 @@ char* strndup(char* src, size_t size)
 	return dst;
 }
 
-void my_qsort(void* first, int number, int size, int (*compar)(void*, void*))
+void my_qsort(void* first, long_int number, size_t size, int (*compar)(void*, void*))
 {
-	int gap, i, j;
+	long_int gap, i, j;
 
  	for (gap = number/2; gap > 0; gap /= 2)
  		for (i = gap; i < number; i++)
@@ -163,7 +214,8 @@ void my_qsort(void* first, int number, int size, int (*compar)(void*, void*))
 }
 
 
-void swap(void* a, void* b, int size)
+
+void swap(void* a, void* b, size_t size)
 {
 	char* tmp = (char*)malloc(size);
 	memcpy(tmp, a, size);
@@ -171,13 +223,14 @@ void swap(void* a, void* b, int size)
 	memcpy(b, tmp, size);
 	free(tmp);
 }
-void output(struct string* ptr_string, size_t num_of_string, FILE* dst)
+void output(string* ptr_string, long_int num_of_string, FILE* dst)
 {
-	for (size_t i = 0; i < num_of_string; ++i)
+	for (long_int i = 0; i < num_of_string; ++i)
 		fprintf(dst, "%s\n", ptr_string[i].line);
 }
-void free_memory(char* f_buffer, struct string* ptr_string, size_t num_of_string)
+void free_memory(char* f_buffer, string* ptr_string, long_int num_of_string)
 {
+
 	free(f_buffer);
 	for (size_t i = 0; i < num_of_string; ++i)
 		free(ptr_string[i].line);
